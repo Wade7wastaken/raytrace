53f6a6ee6dbe03cca6393c7b797f75645a57a530
Wade7wastaken <86390605+Wade7wastaken@users.noreply.github.com>
2025-11-21 21:30:15 -0600

    remote extra files/features

--- src/textures/mod.rs
+++ src/textures/mod.rs
@@ -1,13 +1,5 @@
-mod checker_texture;
-mod image_texture;
-mod noise_texture;
-mod perlin;
-mod rtw_image;
 mod solid_color;
 mod texture;
 
-pub use checker_texture::*;
-pub use image_texture::*;
-pub use noise_texture::*;
 pub use solid_color::*;
 pub use texture::*;
